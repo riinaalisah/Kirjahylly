7346e4a104532aee50764efd1be9173b6b3ef299
Riina-Alisa <riina.alisa@live.com>
2019-01-17 16:23:07 +0200

    Sisäänkirjautumistoiminnallisuuden alku

--- application/auth/views.py
+++ application/auth/views.py
@@ -1,4 +1,5 @@
 from flask import render_template, request, redirect, url_for
+from flask_login import login_user
 
 from application import app
 from application.auth.models import User
@@ -18,5 +19,5 @@ def auth_login():
                     error = "No such username or password")
 
 
-    print("Käyttäjä " + user.name + " tunnistettiin")
+    login_user(user)
     return redirect(url_for("index"))
