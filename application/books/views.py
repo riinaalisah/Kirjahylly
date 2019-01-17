ccd388d57cf4ff479be8226dbfe6cda8505ef7f1
Riina-Alisa <riina.alisa@live.com>
2019-01-17 11:33:08 +0200

    Lisätään kansion books tiedostoon views.py import redirectille

--- application/books/views.py
+++ application/books/views.py
@@ -1,6 +1,10 @@
 from application import app, db
-from flask import render_template, request
+from flask import redirect, render_template, request, url_for
 from application.books.models import Book
+
+@app.route("/books/", methods=["GET"])
+def books_index():
+    return render_template("books/list.html", books = Book.query.all())
 
 @app.route("/books/new/")
 def books_form():
@@ -13,4 +17,4 @@ def books_create():
     db.session().add(b)
     db.session().commit()
 
-    return "Hello world!"
+    return redirect(url_for("books_index"))
--- application/views.py
+++ application/views.py
@@ -1,20 +1,6 @@
-from application import app, db
-from flask import render_template, request, url_for
-from application.books.models import Book
+from flask import render_template
+from application import app
 
-@app.route("/books", methods=["GET"])
-def books_index():
-    return render_template("books/list.html", books = Book.query.all())
-
-@app.route("/books/new/")
-def books_form():
-    return render_template("books/new.html")
-
-@app.route("/books/", methods=["POST"])
-def books_create():
-    b = Book(request.form.get("name"))
-
-    db.session().add(b)
-    db.session().commit()
-
-    return redirect(url_for("books_index"))
+@app.route("/")
+def index():
+    return render_template("index.html")
