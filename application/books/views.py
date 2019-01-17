24de982ca97bab8904f7737746825043e2989949
Riina-Alisa <riina.alisa@live.com>
2019-01-17 11:50:21 +0200

    Päivitetään tiedosto requirements.txt

--- application/books/views.py
+++ application/books/views.py
@@ -10,6 +10,14 @@ def books_index():
 def books_form():
     return render_template("books/new.html")
 
+@app.route("/books/<book_id>/", methods=["POST"])
+def books_set_read(book_id):
+    b = Book.query.get(book_id)
+    b.read = True
+    db.session().commit()
+
+    return redirect(url_for("books_index"))
+
 @app.route("/books/", methods=["POST"])
 def books_create():
     b = Book(request.form.get("name"))
