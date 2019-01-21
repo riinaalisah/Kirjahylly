e614a224bb33e28cad361d00b1361fbd73719c17
Riina-Alisa <riina.alisa@live.com>
2019-01-21 22:49:15 +0200

    Luodaan abstrakti luokka Base, joka sisältää id:n, luomisajankohdan ja päivitysajankohdan. Luokat User, Book ja Author perivät sen.

--- application/authors/models.py
+++ application/authors/models.py
@@ -1,10 +1,7 @@
 from application import db
+from application.models import Base
 
-class Author(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-    onupdate=db.func.current_timestamp())
+class Author(Base):
 
     firstname = db.Column(db.String(144), nullable=False)
     lastname = db.Column(db.String(144), nullable=False)
--- application/books/models.py
+++ application/books/models.py
@@ -1,10 +1,7 @@
 from application import db
+from application.models import Base
 
-class Book(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-		    onupdate=db.func.current_timestamp())
+class Book(Base):
 
     name = db.Column(db.String(144), nullable=False)
     read = db.Column(db.Boolean, nullable=False)
