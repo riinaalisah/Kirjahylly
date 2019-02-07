455da5ba4eafff93b8f3ba8990f16837414e024d
Riina-Alisa <riina.alisa@live.com>
2019-02-07 16:21:50 +0200

    Lisätään lomake käyttäjän tietojen muokkausta varten

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,17 +1,28 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField
+from wtforms import PasswordField, StringField, validators
+
 
 class LoginForm(FlaskForm):
-    username = StringField("Käyttäjänimi")
-    password = PasswordField("Salasana")
+    username = StringField("Käyttäjänimi", [validators.Length(min=2)])
+    password = PasswordField("Salasana", [validators.Length(min=2)])
 
     class Meta:
         csrf = False
 
+
 class UserForm(FlaskForm):
-    name = StringField("Nimi")
-    username = StringField("Käyttäjänimi")
-    password = StringField("Salasana")
+    name = StringField("Nimi", [validators.Length(min=2)])
+    username = StringField("Käyttäjänimi", [validators.Length(min=2)])
+    password = PasswordField("Salasana", [validators.Length(min=5)])
+
+    class Meta:
+        csrf = False
+
+
+class EditForm(FlaskForm):
+    name = StringField("Uusi nimi")
+    username = StringField("Uusi käyttäjänimi")
+    password = PasswordField("Uusi salasana")
 
     class Meta:
         csrf = False
