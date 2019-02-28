380708191918f3af52d1d614b352d792f4a6ae96
Riina-Alisa <riina.alisa@live.com>
2019-02-28 15:39:03 +0200

    Ulkoasumuokkailuja

--- application/models.py
+++ application/models.py
@@ -5,4 +5,4 @@ class Base(db.Model):
     __abstract__ = True
 
     id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_created = db.Column(db.Date, default=db.func.current_date())
